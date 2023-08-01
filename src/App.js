9538ff12aed379c4b8f78fbce1c1e0e70f0fef5d
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2023-08-01 14:56:00 +0200

    Remove comments

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
-
 export default function App() {
     return (
-        <Navbar/>
+        <div>
+            <Navbar/>
+        </div>
     )
 }
