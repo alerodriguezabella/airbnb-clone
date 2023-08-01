2f88eb5d0bada1aa86dc75a500320d7bc06cf978
Alejandra Rodriguez <alerodriguezabella@gmail.com>
2023-08-01 16:02:20 +0200

    Fix Safari logo error

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from "react"
 export default function Navbar() {
     return (
       <nav>
-        <img src={require("../images/airbnb-logo.png")} alt="logo"/>
+        <img src={require("../images/airbnb-logo.png")} alt="logo" className="nav--logo"/>
       </nav>
     )
 }
